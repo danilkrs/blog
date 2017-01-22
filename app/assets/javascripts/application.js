b27b4c9b6be3cb14d278198f60759a7b00fa7457
toxab <toxab@stash.denovolab.com>
2017-01-22 21:05:52 +0200

    test121

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,20 +3,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-$(".nav a").on("click", function(){
-    $(".nav").find(".active").removeClass("active");
-    $(this).parent().addClass("active");
-});
-(function(){
-    $(window).scroll(function () {
-        // значение, на которое сместилась страница при прокрутке
-        var top = $(document).scrollTop();
-        $('.splash').css({
-            //задаём позицию фонового изображения
-            'background-position': '0px -'+(top/3).toFixed(2)+'px'
-        });
-    });
-})();
 
 $(document).ready(function(){
     $("#navbar").on("click","a", function (event) {
@@ -24,5 +10,9 @@ $(document).ready(function(){
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1500);
+    });
+    $(".nav a").on("click", function(){
+        $(".nav").find(".active").removeClass("active");
+        $(this).parent().addClass("active");
     });
 });
