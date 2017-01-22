cdad0ed442633c055ed64be2842f9de6db3a8e46
toxab <toxab@stash.denovolab.com>
2017-01-22 20:16:13 +0200

    test1234

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,8 +3,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require bootstrap
-//= require bootstrap.min
 $(".nav a").on("click", function(){
     $(".nav").find(".active").removeClass("active");
     $(this).parent().addClass("active");
